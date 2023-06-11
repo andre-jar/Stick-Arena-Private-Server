226de21adabbdbe3859f53f88c74f631eb571c99
andre-jar <24274271+andre-jar@users.noreply.github.com>
2023-06-11 16:50:46 +0200

    check for actual version in version_check.php

--- htdocs/stickarena/version_check.php
+++ htdocs/stickarena/version_check.php
@@ -1,1 +1,19 @@
-result=success
+<?php
+if(isset($_POST['ver'])) {
+	$version = $_POST['ver'];
+}
+
+function checkVersion()
+{
+	global $version;
+	if(!isset($version)) {
+		return "result=error";
+	}
+	if($version==598 || $version==588 || $version==558) {
+	   return "result=success";
+	} else {
+	   return "result=error";
+	}
+}
+?>
+<?php echo checkVersion(); ?>
