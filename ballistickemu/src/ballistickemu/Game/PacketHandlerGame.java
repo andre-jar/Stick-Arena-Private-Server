fa277dc7d287c3734a7fd39b8c90a25d6237dd8c
andre-jar <24274271+andre-jar@users.noreply.github.com>
2023-06-15 01:27:18 +0200

    add map rating handler

--- ballistickemu/src/ballistickemu/Game/PacketHandlerGame.java
+++ ballistickemu/src/ballistickemu/Game/PacketHandlerGame.java
@@ -18,102 +18,93 @@
  *     the Free Software Foundation, Inc., 59 Temple Place,
  */
 package ballistickemu.Game;
+
 import ballistickemu.Types.StickClient;
-import ballistickemu.Types.StickRoom;
 import ballistickemu.Lobby.handlers.*;
 import ballistickemu.Game.handlers.*;
+
 /**
  *
  * @author Simon
  */
 public class PacketHandlerGame {
- 
-public static void HandlePacket(String Packet, StickClient client)
-        //public static object HandlePacket(object state)
-        {
-          //  PacketData PD = (PacketData)state;
-            //string Packet = PD.getData();
-            //StickClient client = PD.getClient();
-            if (Packet.length() < 2) { return; }
-           // Console.WriteLine("Packet being handled from " + client.getName() + " : " + Packet);
- 
-                if (Packet.substring(0, 1).equalsIgnoreCase("0"))
-                {
-                            if(Packet.substring(0,2).equalsIgnoreCase("0\0")) {
-                                
-                            } else if(Packet.substring(0,2).equalsIgnoreCase("01"))
-                            {
-                                RoomRequestHandler.handlePacket(client);
 
-                            }
- 
- 
-                            else if(Packet.substring(0,2).equalsIgnoreCase("03"))
-                            {
-                                NewClientHandler.HandlePacket(client, Packet);
+	public static void HandlePacket(String Packet, StickClient client)
+	// public static object HandlePacket(object state)
+	{
+		// PacketData PD = (PacketData)state;
+		// string Packet = PD.getData();
+		// StickClient client = PD.getClient();
+		if (Packet.length() < 2) {
+			return;
+		}
+		// Console.WriteLine("Packet being handled from " + client.getName() + " : " +
+		// Packet);
 
-                            }
- 
-                            else if(Packet.substring(0,2).equalsIgnoreCase("00")) //Send specified data to specified UID
-                            {
-                                GenericSendDataHandler.HandlePacket(client, Packet);
+		if (Packet.substring(0, 1).equalsIgnoreCase("0")) {
+			if (Packet.substring(0, 2).equalsIgnoreCase("0\0")) {
 
-                            }
- 
-                            else if(Packet.substring(0,2).equalsIgnoreCase("04"))
-                            {
-                                RoomDetailRequestHandler.HandlePacket(client, Packet);
+			} else if (Packet.substring(0, 2).equalsIgnoreCase("01")) {
+				RoomRequestHandler.handlePacket(client);
 
-                            }
- 
-                            else if(Packet.substring(0,2).equalsIgnoreCase("05"))
-                            {
-                                SetMapCycleListHandler.HandlePacket(client, Packet);
+			}
 
-                            }
- 
-                            else if(Packet.substring(0,2).equalsIgnoreCase("06"))
-                            {
-                                MapCycleRequestHandler.HandlePacket(client, Packet);
+			else if (Packet.substring(0, 2).equalsIgnoreCase("03")) {
+				NewClientHandler.HandlePacket(client, Packet);
 
-                            }
-                            else if(Packet.substring(0,2).equalsIgnoreCase("0h"))
-                            {
-                                FindRequestHandler.HandlePacket(client, Packet);
+			}
 
-                            }
-                }
- 
-                else if(Packet.substring(0, 1).equalsIgnoreCase("9"))
-                {
-                        GeneralChatHandler.HandlePacket(client, Packet);
+			else if (Packet.substring(0, 2).equalsIgnoreCase("00")) // Send specified data to specified UID
+			{
+				GenericSendDataHandler.HandlePacket(client, Packet);
 
-                }
-                else if(Packet.substring(0,1).equalsIgnoreCase("1") || Packet.substring(0,1).equalsIgnoreCase("2") ||
-                Packet.substring(0,1).equalsIgnoreCase("4") || Packet.substring(0,1).equalsIgnoreCase("6") ||
-                Packet.substring(0,1).equalsIgnoreCase("5") || Packet.substring(0,1).equalsIgnoreCase("8"))
-                {
-                    GamePacketBroadcastHandler.HandlePacket(client, Packet);
-                }
- 
-                else if(Packet.substring(0, 1).equalsIgnoreCase("K"))
-                {
-                    VoteKickHandler.HandlePacket(client, Packet);
-                }
- 
-                else if(Packet.substring(0,1).equalsIgnoreCase("7"))
-                {
-                    KillHandler.HandlePacket(client, Packet);
-                }
- 
-                else
-                {
-                       // Console.WriteLine("Unhandled packet from " + client.getClient().Client.RemoteEndPoint + ":");
-                        System.out.printf("Unhandled packet received by GamePacketHandler: %s", Packet);
-                        System.out.println();
+			}
 
-                }
- 
-            }
- 
-        }
+			else if (Packet.substring(0, 2).equalsIgnoreCase("04")) {
+				RoomDetailRequestHandler.HandlePacket(client, Packet);
+
+			}
+
+			else if (Packet.substring(0, 2).equalsIgnoreCase("05")) {
+				SetMapCycleListHandler.HandlePacket(client, Packet);
+
+			}
+
+			else if (Packet.substring(0, 2).equalsIgnoreCase("06")) {
+				MapCycleRequestHandler.HandlePacket(client, Packet);
+
+			} else if (Packet.substring(0, 2).equalsIgnoreCase("0h")) {
+				FindRequestHandler.HandlePacket(client, Packet);
+			} else if (Packet.substring(0, 2).equalsIgnoreCase("0i")) {
+				MapRatingHandler.HandlePacket(client, Packet);
+			}
+		}
+
+		else if (Packet.substring(0, 1).equalsIgnoreCase("9")) {
+			GeneralChatHandler.HandlePacket(client, Packet);
+
+		} else if (Packet.substring(0, 1).equalsIgnoreCase("1") || Packet.substring(0, 1).equalsIgnoreCase("2")
+				|| Packet.substring(0, 1).equalsIgnoreCase("4") || Packet.substring(0, 1).equalsIgnoreCase("6")
+				|| Packet.substring(0, 1).equalsIgnoreCase("5") || Packet.substring(0, 1).equalsIgnoreCase("8")) {
+			GamePacketBroadcastHandler.HandlePacket(client, Packet);
+		}
+
+		else if (Packet.substring(0, 1).equalsIgnoreCase("K")) {
+			VoteKickHandler.HandlePacket(client, Packet);
+		}
+
+		else if (Packet.substring(0, 1).equalsIgnoreCase("7")) {
+			KillHandler.HandlePacket(client, Packet);
+		}
+
+		else {
+			// Console.WriteLine("Unhandled packet from " +
+			// client.getClient().Client.RemoteEndPoint + ":");
+			System.out.printf("Unhandled packet received by GamePacketHandler: %s", Packet);
+			System.out.println();
+
+		}
+
+	}
+
+}
---
+++ ballistickemu/src/ballistickemu/Game/handlers/MapRatingHandler.java
@@ -0,0 +1,14 @@
+package ballistickemu.Game.handlers;
+
+import ballistickemu.Types.StickClient;
+
+public class MapRatingHandler {
+	public static void HandlePacket(StickClient client, String Packet) {
+		// String rating = Packet.substring(2);
+		// if(!client.getLobbyStatus()) {
+		// String map = client.getRoom().getMapID();
+		// System.out.println("Client " +client.getName() + " rated Map ID:" +map + "
+		// with Rating " +rating);
+		// }
+	}
+}
